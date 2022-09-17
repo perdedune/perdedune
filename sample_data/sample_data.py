abfb66cdeae9ca61f70778f885fea70625c684d4
Marco Germanier <85969120+m-r-g-t@users.noreply.github.com>
2022-09-17 20:16:30 +0100

    further work on normalising api output

--- sample_data/sample_data.py
+++ sample_data/sample_data.py
@@ -1,25 +1,25 @@
 import dotenv
 import os
+import pandas as pd
+#import csv
 
 # this extracts the sample we need to query against within postgres against as a csv.
+query_id_= 1278746
+name_='erc20.ERC20_evt_Transfer'
 
 from dune_client.types import QueryParameter
 from dune_client.client import DuneClient
 from dune_client.query import Query
 
+
 if __name__ == "__main__":
     query = Query(
-        name="Sample Query",
-        query_id=1215383,
+        name=name_,
+        query_id=query_id_,
     )
-    params=[
-        QueryParameter.text_type(name="TextField", value="Word"),
-        QueryParameter.number_type(name="NumberField", value=3.1415926535),
-        QueryParameter.date_type(name="DateField", value="2022-05-04 00:00:00"),
-        QueryParameter.enum_type(name="ListField", value="Option 1"),
-    ],
     dotenv.load_dotenv()
     dune = DuneClient(os.environ["DUNE_API_KEY"])
-    print("Fetched results:", dune.refresh(query))
+    query_res = dune.refresh(query)
 
-    print("Also available at", query.url())
+    df = pd.read_json (query_res)
+    df.to_csv ('.\sample_data\{{}}.csv'.format(name_), index = None)
