81596cc9ca93c670cc790d6d5c7eaa56fecb2c81
Marco Germanier <85969120+m-r-g-t@users.noreply.github.com>
2022-09-18 00:29:55 +0100

    amended pathnames

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sql_from_dashboard import sql_extractor
 
 # enter dashboard url here
-dashboard_url = "https://dune.com/perdedune/perdedune"
+dashboard_url = "https://dune.com/tecommons/TECommons"
 
 # extract unique sql files from dashboard
 sql_extractor(dashboard_url)
