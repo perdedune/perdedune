0b12698cbf9f10265a334ce7c67e970bb26ebd6a
Marco Germanier <85969120+m-r-g-t@users.noreply.github.com>
2022-09-18 02:04:34 +0100

    added args

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from sql_from_dashboard import sql_extractor
+import argparse
 
+args = parser.parse_args()
 # enter dashboard url here
-dashboard_url = "https://dune.com/tecommons/TECommons"
+dashboard_url = args.files[0] #"https://dune.com/perdedune/perdedune"
 
 # extract unique sql files from dashboard
 sql_extractor(dashboard_url)
