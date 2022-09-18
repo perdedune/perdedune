eb1357cd4fc0df5b1664bb184d163f1337280ded
Andrew Chiw <randomshinichi4869@gmail.com>
2022-09-18 02:43:32 +0200

    follow main.py directory scheme

--- find_common_sql.py
+++ find_common_sql.py
@@ -1,32 +1,31 @@
-import difflib
 import argparse
-from os import path
-
-from pprint import pp
-from pathlib import Path
-from os import path
+import difflib
+import glob
 from collections import namedtuple
+from os import path
+from pathlib import Path
+
 import sqlglot
+
 
 def deepp(h, depth=5):
 	pp(h.stmt(depth=depth, skip_none=True))
 
 parser = argparse.ArgumentParser(
-	usage="%(prog)s [OPTION] [FILE]...",
+	usage="%(prog)s PROJECT...",
 	description="prettify SQL statements and check for similar clauses",
 )
-parser.add_argument(
-	"-v", "--version", action="version",
-	version=f"{parser.prog} version 1.0.0"
-)
-parser.add_argument("files", nargs="*")
+parser.add_argument("project", type=str)
 args = parser.parse_args()
 
+# find project_name/input/*.sql files
+files = glob.glob("{}/input/*.sql".format(args.project))
 
 Sql = namedtuple('Sql', ['parsed', 'filename'])
 Abstraction = namedtuple('Abstraction', ['sql', 'alias'])
+
 sqls = []
-for file in args.files:
+for file in files:
 	with open(file) as x:
 		sql = Sql(x.read(), path.basename(file))
 		sqls.append(sql)
@@ -45,7 +44,7 @@ for cte in ctes:
 	with_stmt_removed = [f for f in cte.flatten()][0]
 	abstractions.append(Abstraction(with_stmt_removed.sql(pretty=True), cte.alias))
 
-
+Path("{}/output".format(args.project)).mkdir(parents=True, exist_ok=True)
 for a in abstractions:
-	with open("output/{}.sql".format(a.alias), "w") as f:
+	with open("{}/output/{}.sql".format(args.project, a.alias), "w") as f:
 		f.write(a.sql)
